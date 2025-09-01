8bd4ff44f985fad0aba7619f2ee8b40e5a32701c
nkkkta <comdeegarson@gmail.com>
2025-09-01 10:22:45 +0300

    second point

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+
+import './owl.carousel';
+import './owl.carousel.min';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.querySelector('.mobile-menu');
+    const mainLinks = document.querySelector('.pre-header__left');
+
+    toggleButton.addEventListener('click', () => {
+        mainLinks.classList.toggle('is-open');
+    });
+});
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
