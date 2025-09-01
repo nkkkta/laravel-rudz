76d84586947ee7255707ff08d08ac56e2c6ac5ff
nkkkta <comdeegarson@gmail.com>
2025-09-01 14:14:29 +0300

    sliders

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,17 +1,34 @@
 
-import './owl.carousel';
-import './owl.carousel.min';
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
 
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const toggleButton = document.querySelector('.mobile-menu');
     const mainLinks = document.querySelector('.pre-header__left');
 
     toggleButton.addEventListener('click', () => {
         mainLinks.classList.toggle('is-open');
     });
-});
 
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+    await import('./owl.carousel.js');
+
+    $(function() {
+        $(".owl-carousel").owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            dots:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:2
+                }
+            }
+        });
+    });
 });
